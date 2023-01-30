e3ef8d3317909780169f5195289d47fa8948e881
eskokado <eskokado@gmail.com>
2023-01-30 07:10:04 -0300

    test: ensure search element(75) result not found

--- spec/algorithm/type_spec.rb
+++ spec/algorithm/type_spec.rb
@@ -11,4 +11,16 @@ describe "Searching Algorithm" do
     end    
     expect(message).to eq("Target element is found")
   end  
+  it "Ensure Search element(75) result not found" do
+    array = [10, 50, 30, 70, 80, 60, 20, 90, 40]
+    valueSearch = 75
+    message = "Element is not found"
+    for value in array do
+      if value == valueSearch 
+        message = "Target element is found"
+        break
+      end
+    end    
+    expect(message).to eq("Element is not found")
+  end  
 end
