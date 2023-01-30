85f680ad217b2aa6db2c69fbff29f244cc70461e
eskokado <eskokado@gmail.com>
2023-01-30 07:53:34 -0300

    refactor: organizing folders and files

---
+++ spec/algorithm/type/search_algorithm/linear_search_spec.rb
@@ -0,0 +1,54 @@
+describe "Linear Search" do
+  it "ensure search element 20 is found" do
+    array = [10, 50, 30, 70, 80, 60, 20, 90, 40]
+    valueSearch = 20
+    message = "Element is not found"
+    for value in array do
+      if value == valueSearch 
+        message = "Target element is found"
+        break
+      end
+    end    
+    expect(message).to eq("Target element is found")
+  end  
+  it "Ensure Search element(75) result not found" do
+    array = [10, 50, 30, 70, 80, 60, 20, 90, 40]
+    valueSearch = 75
+    message = "Element is not found"
+    for value in array do
+      if value == valueSearch 
+        message = "Target element is found"
+        break
+      end
+    end    
+    expect(message).to eq("Element is not found")
+  end  
+  it "ensure found 110 in array result index and element" do
+    array = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+    valueSearch = 110
+    findIndex = -1
+    message = "Element 110 is not present in array"
+    array.each_with_index do |value, index|
+      if value == valueSearch 
+        findIndex = index
+        message = "Element #{valueSearch} is present at index #{index}"
+        break
+      end
+    end    
+    expect(message).to eq("Element 110 is present at index 6")
+  end  
+  it "ensure search by element 175 is not present in array" do
+    array = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+    valueSearch = 175
+    findIndex = -1
+    message = "Element 175 is not present in array[]"
+    array.each_with_index do |value, index|
+      if value == valueSearch 
+        findIndex = index
+        message = "Element #{valueSearch} is present at index #{index}"
+        break
+      end
+    end    
+    expect(message).to eq("Element 175 is not present in array[]")
+  end  
+end
