5b88655c3d365099a93d67fc1a0f64a8022e1f73
eskokado <eskokado@gmail.com>
2023-01-31 06:13:17 -0300

    test: add sorting reverse in sorting algorithms

--- spec/algorithm/type/sorting_algorithms/bubble_sort_spec.rb
+++ spec/algorithm/type/sorting_algorithms/bubble_sort_spec.rb
@@ -4,4 +4,8 @@ describe "Bubble Sort" do
     result = array.sort
     expect(result).to eq([1, 2, 4, 5, 8])
   end
+  it "Sorting reverse" do
+    result = array.sort {|a, b| b <=> a }
+    expect(result).to eq([8, 5, 4, 2, 1])
+  end
 end
