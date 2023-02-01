f2a733d700d8ff3bcdb042ebb6d68e329f63204b
eskokado <eskokado@gmail.com>
2023-02-01 07:48:16 -0300

    feat: add method main in KruskalsMST

--- spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
+++ spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
@@ -1,4 +1,18 @@
 class KruskalsMST
+  def main()
+    @V = 4
+    @graphEdges = [
+      Edge.new(0, 1, 10),
+      Edge.new(0, 2, 6),
+      Edge.new(0, 3, 5),
+      Edge.new(1, 3, 5),
+      Edge.new(1, 3, 15),
+      Edge.new(2, 3, 4)
+    ]
+    @graphEdges.sort {|a,b| a.weight <=> b.weight}
+  
+    kruskals(@V, @graphEdges)
+  end
 end
 
 class Edge < KruskalsMST
@@ -16,6 +30,18 @@ class Subset
     @parent = parent
     @rank = rank
   end
+end
+
+def kruskals(v, edges)
+  @j = 0
+  @noOfEdges = 0
+
+  @subsets = []
+  for i in 0..(v-1)
+    @subsets.push(Subset.new(i, 0))
+  end
+  puts "Funcionando function kruskals"
+  puts @subsets
 end
 
 describe "Minimum Spanning" do
