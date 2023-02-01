1385b573e370860bcd5d1ba8ae77e97b640c385d
eskokado <eskokado@gmail.com>
2023-02-01 08:33:04 -0300

    feat: completed the class with error

--- spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
+++ spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
@@ -40,8 +40,61 @@ def kruskals(v, edges)
   for i in 0..(v-1)
     @subsets.push(Subset.new(i, 0))
   end
+
+  @results = []
+  for i in 0..(v-1)
+    @results.push(Edge.new(0, 0, 0))
+  end
+
   puts "Funcionando function kruskals"
   puts @subsets
+  puts @results
+
+  while @noOfEdges < (v - 1)
+    @nextEdge = edges[@j]
+    @x = findRoot(@subsets, @nextEdge.src)
+    @y = findRoot(@subsets, @nextEdge.dest)
+
+    if (@x != @y)
+      @results[@noOfEdges] = @nextEdge
+      union(@subsets, @x, @y)
+      @noOfEdges += 1
+    end
+    @j += 1
+  end
+
+  puts "Following are the edges of the constructed MST:"
+  puts "-----------------------------------------------"
+  @minCost = 0
+  for i in 0..@noOfEdges do
+    puts "#{@results[i].src} - #{@results[i].dest} : #{@results[i].weight}"
+    @minCost += @results[i].weight
+  end
+  puts "-----------------------------------------------"
+  puts "Total cost of MST: #{@minCost}"
+end
+
+def union(subsets, x, y)
+  rootX = findRoot(subsets, x)
+  rootY = findRoot(subsets, y)
+
+  if (subsets[rootY].rank < subsets[rootX].rank)
+    subsets[rootY].parent = rootX
+  elsif (subsets[rootX].rank < subsets[rootY].rank)
+    subsets[rootX].parent = rootY
+  else
+    subsets[rootY].parent = rootX
+    subsets[rootX].rank += 1
+  end
+end
+
+def findRoot(subsets, i)
+  if (subsets[i].parent == i) 
+    return subsets[i].parent
+  end
+  
+  subsets[i].parent = findRoot(subsets, subsets[i].parent)
+  return subsets.parent
 end
 
 describe "Minimum Spanning" do
