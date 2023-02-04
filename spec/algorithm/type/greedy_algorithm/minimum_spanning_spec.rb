84c2042f9a11eb5e1e81a9cd9f4fb76361ab491f
eskokado <eskokado@gmail.com>
2023-02-03 21:55:08 -0300

    test: minimum spanning spec - part final with error

--- spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
+++ spec/algorithm/type/greedy_algorithm/minimum_spanning_spec.rb
@@ -31,6 +31,45 @@ class Graph
     end
   end
 
+  def KruskalMST
+    result = []
+    for i in 0..(@V - 1)
+      result.push(Edge.new)
+    end
+    @edge.sort_by! {|e| e.weight}
+
+    subsets = []
+    for i in 0..(@V - 1)
+      subsets.push(Subset.new)
+      subsets[i].parent = i
+      subsets[i].rank = 0
+    end
+    i = 0
+    e = 0
+    while e < (@V - 1) do
+      next_edge = Edge.new
+      next_edge = edge[i]
+
+      x = find(subsets, next_edge.src)
+      y = find(subsets, next_edge.dest)
+
+      if x != y
+        result[e] = next_edge
+        Union(subsets, x, y)
+        e += 1
+      end
+      i += 1
+    end
+
+    puts "Following are the edges in the constructed MST"
+    minimumCost = 0
+    for i in 0.. e - 1
+      puts "#{result[i].src} -- #{result[i].dest} == #{result[i].weight}"
+      minimumCost += result[i].weight
+    end
+    puts "Minimum Cost Spanning Tree: #{minimumCost}"
+  end
+
 end
 
 class Edge
@@ -41,108 +80,39 @@ class Subset
   attr_accessor :parent, :rank
 end
 
-# class KruskalsMST
-#   def main()
-#     @V = 4
-#     @graphEdges = [
-#       Edge.new(0, 1, 10),
-#       Edge.new(0, 2, 6),
-#       Edge.new(0, 3, 5),
-#       Edge.new(1, 3, 5),
-#       Edge.new(1, 3, 15),
-#       Edge.new(2, 3, 4)
-#     ]
-#     @graphEdges.sort {|a,b| a.weight <=> b.weight}
-#
-#     kruskals(@V, @graphEdges)
-#   end
-# end
-#
-# class Edge < KruskalsMST
-#   attr_accessor :src, :dest, :weight
-#   def initialize(src, dest, weight)
-#     @src = src
-#     @dest = dest
-#     @weight = weight
-#   end
-# end
-#
-# class Subset
-#   attr_accessor :parent, :rank
-#   def initialize(parent, rank)
-#     @parent = parent
-#     @rank = rank
-#   end
-# end
-#
-# def kruskals(v, edges)
-#   @j = 0
-#   @noOfEdges = 0
-#
-#   @subsets = []
-#   for i in 0..(v-1)
-#     @subsets.push(Subset.new(i, 0))
-#   end
-#
-#   @results = []
-#   for i in 0..(v-1)
-#     @results.push(Edge.new(0, 0, 0))
-#   end
-#
-#   puts "Funcionando function kruskals"
-#   puts @subsets
-#   puts @results
-#
-#   while @noOfEdges < (v - 1)
-#     @nextEdge = edges[@j]
-#     @x = findRoot(@subsets, @nextEdge.src)
-#     @y = findRoot(@subsets, @nextEdge.dest)
-#
-#     if (@x != @y)
-#       @results[@noOfEdges] = @nextEdge
-#       union(@subsets, @x, @y)
-#       @noOfEdges += 1
-#     end
-#     @j += 1
-#   end
-#
-#   puts "Following are the edges of the constructed MST:"
-#   puts "-----------------------------------------------"
-#   @minCost = 0
-#   for i in 0..@noOfEdges do
-#     puts "#{@results[i].src} - #{@results[i].dest} : #{@results[i].weight}"
-#     @minCost += @results[i].weight
-#   end
-#   puts "-----------------------------------------------"
-#   puts "Total cost of MST: #{@minCost}"
-# end
-#
-# def union(subsets, x, y)
-#   rootX = findRoot(subsets, x)
-#   rootY = findRoot(subsets, y)
-#
-#   if (subsets[rootY].rank < subsets[rootX].rank)
-#     subsets[rootY].parent = rootX
-#   elsif (subsets[rootX].rank < subsets[rootY].rank)
-#     subsets[rootX].parent = rootY
-#   else
-#     subsets[rootY].parent = rootX
-#     subsets[rootX].rank += 1
-#   end
-# end
-#
-# def findRoot(subsets, i)
-#   if (subsets[i].parent == i)
-#     return subsets[i].parent
-#   end
-#
-#   subsets[i].parent = findRoot(subsets, subsets[i].parent)
-#   return subsets.parent
-# end
-#
-# describe "Minimum Spanning" do
-#   it "description" do
-#     @k = KruskalsMST.new
-#     @k.main()
-#   end
-# end
+describe "Minimum Spanning" do
+  it "description" do
+    # Number of vertices in graph
+    v = 4
+    # Number of edges in graph
+    e = 5
+    graph = Graph.new(v, e)
+
+    # add edge 0-1
+    graph.edge[0].src = 0
+    graph.edge[0].dest = 1
+    graph.edge[0].weight = 10
+
+    # add edge 0-2
+    graph.edge[1].src = 0
+    graph.edge[1].dest = 2
+    graph.edge[1].weight = 6
+
+    # add edge 0-3
+    graph.edge[2].src = 0
+    graph.edge[2].dest = 3
+    graph.edge[2].weight = 5
+
+    # add edge 1-3
+    graph.edge[3].src = 1
+    graph.edge[3].dest = 3
+    graph.edge[3].weight = 5
+
+    # add edge 2-3
+    graph.edge[4].src = 2
+    graph.edge[4].dest = 3
+    graph.edge[4].weight = 4
+
+    graph.KruskalMST()
+  end
+end
