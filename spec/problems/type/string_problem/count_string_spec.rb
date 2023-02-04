52877293e615bdf3850586545884638c5b22bf1e
eskokado <eskokado@gmail.com>
2023-02-04 08:36:03 -0300

    test: Count lower letter in an string

--- spec/problems/type/string_problem/count_string_spec.rb
+++ spec/problems/type/string_problem/count_string_spec.rb
@@ -8,10 +8,26 @@ def count_upper(str)
   result
 end
 
+def count_lower(str)
+  result = 0
+  for i in 0..(str.size - 1)
+    if str[i] >= 'a' && str[i] <= 'z'
+      result += 1
+    end
+  end
+  result
+end
+
+
 describe "Count string" do
   str = "#GeeKs01fOr@gEEks07"
   it "Ensure count char upper in string" do
     upper = count_upper(str)
     expect(upper).to eq(5)
+  end
+
+  it "Ensure count char lower in string" do
+    upper = count_lower(str)
+    expect(upper).to eq(8)
   end
 end
